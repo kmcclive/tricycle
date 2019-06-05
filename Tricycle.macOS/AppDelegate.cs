5e1c2780e2cc6959b696286a2d3cb6fb51160158
Kenny McClive <kenny.mcclive@slalom.com>
2019-06-05 16:07:26 -0600

    Initial commit

---
+++ Tricycle.macOS/AppDelegate.cs
@@ -0,0 +1,34 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace Tricycle.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+            var rect = new CoreGraphics.CGRect(200, 200, 800, 600);
+
+            MainWindow = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            MainWindow.Title = "Tricycle";
+        }
+
+        public override NSWindow MainWindow { get; }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
