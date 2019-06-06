b684e0a813eb8d39df0501f747b9746e3db87494
Kenny McClive <kenny.mcclive@slalom.com>
2019-06-06 15:30:37 -0600

    Implemented ProcessWrapper.

---
+++ Tricycle.Diagnostics/IProcess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Tricycle.Diagnostics
+{
+    /// <summary>
+    /// Provides access to system processes.
+    /// </summary>
+    public interface IProcess : IDisposable
+    {
+        /// <summary>
+        /// Starts the process.
+        /// </summary>
+        /// <returns>The start.</returns>
+        /// <param name="startInfo">The info used to start the process.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="startInfo"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="startInfo"/> is invalid.</exception>
+        /// <exception cref="InvalidOperationException">An error occurred starting the process.</exception>
+        bool Start(ProcessStartInfo startInfo);
+
+        /// <summary>
+        /// Kill the process.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">An error occurred killing the process.</exception>
+        void Kill();
+
+        /// <summary>
+        /// Occurs when the process exits.
+        /// </summary>
+        event Action Exited;
+
+        /// <summary>
+        /// Occurs when error data is received.
+        /// </summary>
+        event Action<string> ErrorDataReceived;
+
+        /// <summary>
+        /// Occurs when output data is received.
+        /// </summary>
+        event Action<string> OutputDataReceived;
+    }
+}
---
+++ Tricycle.Diagnostics/ProcessWrapper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+
+namespace Tricycle.Diagnostics
+{
+    /// <summary>
+    /// Wraps <see cref="Process"/> so that it implements <see cref="IProcess"/>.
+    /// </summary>
+    /// <seealso cref="IProcess"/>
+    public class ProcessWrapper : IProcess
+    {
+        readonly Process _process;
+
+        public event Action Exited;
+        public event Action<string> ErrorDataReceived;
+        public event Action<string> OutputDataReceived;
+
+        public ProcessWrapper()
+        {
+            _process = new Process()
+            {
+                EnableRaisingEvents = true
+            };
+
+            _process.Exited += (sender, e) => Exited?.Invoke();
+            _process.ErrorDataReceived += (sender, e) => ErrorDataReceived?.Invoke(e?.Data);
+            _process.OutputDataReceived += (sender, e) => OutputDataReceived?.Invoke(e?.Data);
+        }
+
+        public void Dispose()
+        {
+            _process.Dispose();
+        }
+
+        public void Kill()
+        {
+            try
+            {
+                _process.Kill();
+            }
+            catch (NotSupportedException ex)
+            {
+                throw new InvalidOperationException("An error occurred killing the process.", ex);
+            }
+            catch (Win32Exception ex)
+            {
+                throw new InvalidOperationException("An error occurred killing the process.", ex);
+            }
+        }
+
+        public bool Start(ProcessStartInfo startInfo)
+        {
+            _process.StartInfo = startInfo;
+
+            try
+            {
+                return _process.Start();
+            }
+            catch (ObjectDisposedException ex)
+            {
+                throw new InvalidOperationException("An error occurred starting the process.", ex);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new ArgumentException($"{nameof(startInfo)} is invalid.", ex);
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new InvalidOperationException("An error occurred starting the process.", ex);
+            }
+            catch (Win32Exception ex)
+            {
+                throw new InvalidOperationException("An error occurred starting the process.", ex);
+            }
+        }
+    }
+}
