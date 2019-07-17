e2717b5fc7a99204d408872fe81b0a40f0991a28
Kenny McClive <kenny.mcclive@slalom.com>
2019-07-17 11:46:56 -0600

    Defined IMediaTranscoder interface.

---
+++ Tricycle.Media/IMediaTranscoder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Tricycle.Models.Jobs;
+
+namespace Tricycle.Media
+{
+    public interface IMediaTranscoder
+    {
+        /// <summary>
+        /// Gets a value indicating whether a job is running.
+        /// </summary>
+        bool IsRunning { get; }
+
+        /// <summary>
+        /// Occurs when the status changes.
+        /// </summary>
+        event Action<TranscodeStatus> StatusChanged;
+
+        /// <summary>
+        /// Occurs when the job completes.
+        /// </summary>
+        event Action Completed;
+
+        /// <summary>
+        /// Occurs when the job fails.
+        /// </summary>
+        event Action<string> Failed;
+
+        /// <summary>
+        /// Starts a specified transcode job.
+        /// </summary>
+        /// <param name="job">The job to start.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="job"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="job"/> is incomplete or invalid.</exception>
+        /// <exception cref="NotSupportedException"><paramref name="job"/> contains unsupported options.</exception>
+        /// <exception cref="InvalidOperationException">
+        /// An error occurred starting the job
+        /// OR
+        /// a job is already running.
+        /// </exception>
+        void Start(TranscodeJob job);
+
+        /// <summary>
+        /// Stops the currently-running job.
+        /// </summary>
+        void Stop();
+    }
+}
---
+++ Tricycle.Models/Jobs/TranscodeStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Tricycle.Models.Jobs
+{
+    public class TranscodeStatus
+    {
+        public double ProgressPercent { get; set; }
+        public TimeSpan ProgressTime { get; set; }
+        public double RateFps { get; set; }
+        public double RelativeRate { get; set; }
+    }
+}
