34d20c8aaed2c8253ccefc9d9b2b184857f3bd92
Kenny McClive <kenny.mcclive@me.com>
2019-10-14 13:41:30 -0600

    Created Windows projects

---
+++ Tricycle.IO.Windows/FileBrowser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tricycle.IO.Models;
+
+namespace Tricycle.IO.Windows
+{
+    public class FileBrowser : IFileBrowser
+    {
+        public Task<FileBrowserResult> BrowseToOpen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileBrowserResult> BrowseToOpen(string defaultDirectory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileBrowserResult> BrowseToOpen(string defaultDirectory, IList<string> extensions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileBrowserResult> BrowseToSave()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileBrowserResult> BrowseToSave(string defaultDirectory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileBrowserResult> BrowseToSave(string defaultDirectory, string defaultFileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tricycle.UI.Windows/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace Tricycle.UI.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Forms.Init();
+            LoadApplication(new UI.App());
+        }
+    }
+}
