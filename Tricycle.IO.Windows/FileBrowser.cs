6ab8cc8a1ccc9540696e3a2f94a874ca6d4917c4
Kenny McClive <kenny.mcclive@me.com>
2019-10-14 19:53:40 -0600

    Implemented FIleBrowser for WIndows.

--- Tricycle.IO.Windows/FileBrowser.cs
+++ Tricycle.IO.Windows/FileBrowser.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 using Tricycle.IO.Models;
 
 namespace Tricycle.IO.Windows
@@ -9,32 +12,76 @@ namespace Tricycle.IO.Windows
     {
         public Task<FileBrowserResult> BrowseToOpen()
         {
-            throw new NotImplementedException();
+            return BrowseToOpen(null);
         }
 
         public Task<FileBrowserResult> BrowseToOpen(string defaultDirectory)
         {
-            throw new NotImplementedException();
+            return BrowseToOpen(defaultDirectory, null);
         }
 
         public Task<FileBrowserResult> BrowseToOpen(string defaultDirectory, IList<string> extensions)
         {
-            throw new NotImplementedException();
+            var result = new FileBrowserResult();
+            var dialog = new OpenFileDialog()
+            {
+                InitialDirectory = defaultDirectory
+            };
+
+            if (extensions?.Any() == true)
+            {
+                var filterBuilder = new StringBuilder();
+
+                for (int i = 0; i < extensions.Count; i++)
+                {
+                    if (i > 0)
+                    {
+                        filterBuilder.Append("|");
+                    }
+
+                    string ext = extensions[i].Replace(".", string.Empty);
+
+                    filterBuilder.Append($"{ext.ToUpper()} Files (*.{ext})|*.{ext}");
+                }
+
+                dialog.Filter = filterBuilder.ToString();
+            }
+
+            if (dialog.ShowDialog() == true)
+            {
+                result.Confirmed = true;
+                result.FileName = dialog.FileName;
+            }
+
+            return Task.FromResult(result);
         }
 
         public Task<FileBrowserResult> BrowseToSave()
         {
-            throw new NotImplementedException();
+            return BrowseToSave(null);
         }
 
         public Task<FileBrowserResult> BrowseToSave(string defaultDirectory)
         {
-            throw new NotImplementedException();
+            return BrowseToSave(defaultDirectory, null);
         }
 
         public Task<FileBrowserResult> BrowseToSave(string defaultDirectory, string defaultFileName)
         {
-            throw new NotImplementedException();
+            var result = new FileBrowserResult();
+            var dialog = new SaveFileDialog()
+            {
+                InitialDirectory = defaultDirectory,
+                FileName = defaultFileName
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                result.Confirmed = true;
+                result.FileName = dialog.FileName;
+            }
+
+            return Task.FromResult(result);
         }
     }
 }
