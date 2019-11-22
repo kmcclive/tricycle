b995f7dc2ed4ceb567dffba073a090ba63cf781e
Kenny McClive <kenny.mcclive@me.com>
2019-11-22 15:26:37 -0700

    Added menu to Windows app.

--- Tricycle.UI.Windows/MainWindow.xaml.cs
+++ Tricycle.UI.Windows/MainWindow.xaml.cs
@@ -2,6 +2,10 @@
 using System.Diagnostics;
 using System.IO;
 using System.IO.Abstractions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
 using StructureMap;
 using Tricycle.Diagnostics;
 using Tricycle.Diagnostics.Utilities;
@@ -12,9 +16,14 @@ using Tricycle.Media.FFmpeg;
 using Tricycle.Media.FFmpeg.Models;
 using Tricycle.Models;
 using Tricycle.Models.Config;
+using Tricycle.UI.Views;
 using Tricycle.Utilities;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.WPF;
+using Xamarin.Forms.Platform.WPF.Controls;
+using ControlTemplate = System.Windows.Controls.ControlTemplate;
+using Menu = System.Windows.Controls.Menu;
+using MenuItem = System.Windows.Controls.MenuItem;
 
 namespace Tricycle.UI.Windows
 {
@@ -24,6 +33,7 @@ namespace Tricycle.UI.Windows
     public partial class MainWindow : FormsApplicationPage
     {
         IAppManager _appManager;
+        ConfigurationPage _configurationPage;
 
         public MainWindow()
         {
@@ -33,6 +43,96 @@ namespace Tricycle.UI.Windows
             InitializeComponent();
             Forms.Init();         
             LoadApplication(new UI.App());
+        }
+
+        protected override void OnTemplateChanged(ControlTemplate oldTemplate, ControlTemplate newTemplate)
+        {
+            base.OnTemplateChanged(oldTemplate, newTemplate);
+
+            Dispatcher.BeginInvoke(DispatcherPriority.DataBind, new Action(CreateMenu));
+        }
+
+        void CreateMenu()
+        {
+            var contentContainer = Template.FindName("PART_ContentControl", this) as FormsContentControl;
+
+            if (contentContainer == null)
+            {
+                return;
+            }
+
+            var panel = new StackPanel();
+            var brush = new SolidColorBrush(Colors.WhiteSmoke);
+            var menu = new Menu()
+            {
+                IsMainMenu = true,
+                Background = brush,
+                Padding = new System.Windows.Thickness(10, 5, 10, 5),
+                FontSize = 14,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Top
+            };
+            var content = contentContainer.Content as UIElement;
+
+            contentContainer.Content = panel;
+
+            panel.Children.Add(menu);
+            panel.Children.Add(content);
+
+            var fileItem = new MenuItem()
+            {
+                Background = brush,
+                Header = "_File"
+            };
+
+            menu.Items.Add(fileItem);
+
+            var openItem = new MenuItem()
+            {
+                Background = brush,
+                Header = "_Open"
+            };
+
+            openItem.Click += OnOpenFileClick;
+            fileItem.Items.Add(openItem);
+
+            var toolsItem = new MenuItem()
+            {
+                Background = brush,
+                Header = "_Tools"
+            };
+
+            menu.Items.Add(toolsItem);
+
+            var optionsItem = new MenuItem()
+            {
+                Background = brush,
+                Header = "_Options"
+            };
+
+            optionsItem.Click += OnOptionsClick;
+            toolsItem.Items.Add(optionsItem);
+        }
+
+        void OnOpenFileClick(object sender, RoutedEventArgs e)
+        {
+            var browser = new FileBrowser();
+            var result = browser.BrowseToOpen().GetAwaiter().GetResult();
+
+            if (result.Confirmed)
+            {
+                _appManager.RaiseFileOpened(result.FileName);
+            }
+        }
+
+        private void OnOptionsClick(object sender, RoutedEventArgs e)
+        {
+            if (_configurationPage == null)
+            {
+                _configurationPage = new ConfigurationPage();
+            }
+
+            _appManager.RaiseModalOpened(_configurationPage);
         }
 
         void InitializeAppState()
