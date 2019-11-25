f55662f014bdeb2b955840e8ff7cd1936d54ee87
Kenny McClive <kenny.mcclive@me.com>
2019-11-25 16:18:40 -0700

    Started creating VideoConfigSection control.

---
+++ Tricycle.UI/Controls/VideoConfigSection.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Tricycle.UI.Controls
+{
+    public partial class VideoConfigSection : ContentView
+    {
+        public VideoConfigSection()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Tricycle.UI/Views/ConfigPage.xaml.cs
+++ Tricycle.UI/Views/ConfigPage.xaml.cs
@@ -1,26 +1,63 @@
 ﻿using System;
-
+using System.Linq;
 using Xamarin.Forms;
 
 namespace Tricycle.UI.Views
 {
     public partial class ConfigPage : ContentPage
     {
+        enum Section
+        {
+            General,
+            Video,
+            Audio,
+            Advanced
+        }
+
         public ConfigPage()
         {
             InitializeComponent();
 
-            var sections = new string[] { "General", "Advanced" };
+            var sections = Enum.GetValues(typeof(Section)).Cast<Section>().ToArray();
+            var selectedSection = sections[0];
 
             vwSections.ItemsSource = sections;
-            vwSections.SelectedItem = sections[0];
+            vwSections.SelectedItem = selectedSection;
 
-            btnClose.Clicked += onClose;
+            SelectSection(selectedSection);
+
+            vwSections.ItemSelected += OnSectionSelected;
+            btnClose.Clicked += OnClose;
         }
 
-        async void onClose(object sender, EventArgs e)
+        void OnSectionSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            SelectSection((Section)e.SelectedItem);
+        }
+
+        async void OnClose(object sender, EventArgs e)
         {
             await Navigation.PopModalAsync();
+        }
+
+        void SelectSection(Section section)
+        {
+            pnlSection.Title = section.ToString();
+
+            foreach (var child in stackSections.Children)
+            {
+                child.IsVisible = false;
+            }
+
+            switch (section)
+            {
+                case Section.General:
+                    sctGeneral.IsVisible = true;
+                    break;
+                case Section.Video:
+                    sctVideo.IsVisible = true;
+                    break;
+            }
         }
     }
 }
