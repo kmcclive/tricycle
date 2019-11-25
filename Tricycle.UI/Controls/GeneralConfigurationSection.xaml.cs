e3aa24c5f89856b6603634006fab4fb37bc4d86b
Kenny McClive <kenny.mcclive@me.com>
2019-11-25 14:42:12 -0700

    Stubbed out controls for general configuration section.

---
+++ Tricycle.UI/Controls/GeneralConfigurationSection.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Tricycle.UI.Controls
+{
+    public partial class GeneralConfigurationSection : ContentView
+    {
+        public GeneralConfigurationSection()
+        {
+            InitializeComponent();
+        }
+    }
+}
