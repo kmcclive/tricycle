e9443676954a7419a882d81e7215119acfc0822c
Kenny McClive <kenny.mcclive@me.com>
2019-11-25 10:50:13 -0700

    Created FramedPanel control.

---
+++ Tricycle.UI/Controls/FramedPanel.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace Tricycle.UI.Controls
+{
+    public partial class FramedPanel : ContentView
+    {
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(
+          nameof(Title),
+          typeof(string),
+          typeof(FramedPanel));
+
+        public string Title
+        {
+            get { return GetValue(TitleProperty).ToString(); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public FramedPanel()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            switch (propertyName)
+            {
+                case nameof(Title):
+                    if (GetTemplateChild("lblTitle") is Label lblTitle)
+                    {
+                        lblTitle.Text = Title;
+                    }
+                    break;
+            }
+        }
+    }
+}
