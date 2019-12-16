8db1405c5e237cfe27641cf3669fbabb8b45da28
Kenny McClive <kenny.mcclive@me.com>
2019-12-16 10:24:51 -0700

    Created GradientStackLayout view for macOS.

---
+++ Tricycle.UI.macOS/Renderers/GradientStackLayoutRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using CoreAnimation;
+using CoreGraphics;
+using Tricycle.UI.macOS.Renderers;
+using Tricycle.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+[assembly: ExportRenderer(typeof(GradientStackLayout), typeof(GradientStackLayoutRenderer))]
+namespace Tricycle.UI.macOS.Renderers
+{
+    public class GradientStackLayoutRenderer : VisualElementRenderer<StackLayout>
+    {
+        public override void DrawRect(CGRect dirtyRect)
+        {
+            base.DrawRect(dirtyRect);
+
+            var layout = (GradientStackLayout)this.Element;
+            var gradient = new CAGradientLayer()
+            {
+                Frame = dirtyRect,
+                BorderColor = layout.BorderColor.ToCGColor(),
+                BorderWidth = (nfloat)layout.BorderWidth,
+                Colors = layout.GradientColors?.Reverse().Select(c => c.ToCGColor()).ToArray()
+            };
+
+            if (layout.GradientOrientation == StackOrientation.Horizontal)
+            {
+                gradient.StartPoint = new CGPoint(0, 0.5);
+                gradient.EndPoint = new CGPoint(1, 0.5);
+            }
+            else
+            {
+                gradient.StartPoint = new CGPoint(0.5, 0);
+                gradient.EndPoint = new CGPoint(0.5, 1);
+            }
+
+            NativeView.Layer.InsertSublayer(gradient, 0);
+        }
+    }
+}
---
+++ Tricycle.UI/Views/GradientStackLayout.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Tricycle.UI.Views
+{
+    public class GradientStackLayout : StackLayout
+    {
+        public static readonly BindableProperty BorderColorProperty = BindableProperty.Create(
+          nameof(BorderColor),
+          typeof(Color),
+          typeof(GradientStackLayout));
+
+        public static readonly BindableProperty BorderWidthProperty = BindableProperty.Create(
+          nameof(BorderWidthProperty),
+          typeof(double),
+          typeof(GradientStackLayout));
+
+        public static readonly BindableProperty GradientColorsProperty = BindableProperty.Create(
+          nameof(GradientColors),
+          typeof(ICollection<Color>),
+          typeof(GradientStackLayout));
+
+        public static readonly BindableProperty GradientOrientationProperty = BindableProperty.Create(
+          nameof(GradientOrientation),
+          typeof(StackOrientation),
+          typeof(GradientStackLayout));
+
+        public Color BorderColor
+        {
+            get { return (Color)GetValue(BorderColorProperty); }
+            set { SetValue(BorderColorProperty, value); }
+        }
+
+        public double BorderWidth
+        {
+            get { return (double)GetValue(BorderWidthProperty); }
+            set { SetValue(BorderWidthProperty, value); }
+        }
+
+        public ICollection<Color> GradientColors
+        {
+            get { return (ICollection<Color>)GetValue(GradientColorsProperty); }
+            set { SetValue(GradientColorsProperty, value); }
+        }
+
+        public StackOrientation GradientOrientation
+        {
+            get { return (StackOrientation)GetValue(GradientOrientationProperty); }
+            set { SetValue(GradientOrientationProperty, value); }
+        }
+
+        public GradientStackLayout()
+        {
+            SetValue(GradientColorsProperty, new List<Color>());
+        }
+    }
+}
