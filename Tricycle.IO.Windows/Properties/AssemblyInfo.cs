2e22a7a3d9c459eb4c7591afa365792243afdfb0
Kenny McClive <kenny.mcclive@me.com>
2020-01-18 14:39:37 -0700

    Updated version to 2.1.0 on Windows side.

--- Tricycle.IO.Windows/Properties/AssemblyInfo.cs
+++ Tricycle.IO.Windows/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tricycle.IO.Windows")]
-[assembly: AssemblyCopyright("Copyright © 2019 Tricycle Team")]
+[assembly: AssemblyCopyright("Copyright © 2020 Tricycle Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
