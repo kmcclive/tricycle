f857e79b102cd09e1e831c03b3fbff1d6aae1144
Kenny McClive <kenny.mcclive@me.com>
2020-02-22 16:41:02 -0700

    Implemented AppManager for Windows.

---
+++ Tricycle.UI.Windows/AppManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace Tricycle.UI.Windows
+{
+    class AppManager : AppManagerBase
+    {
+        Window _mainWindow;
+
+        public AppManager(Window mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+
+        public override void Alert(string title, string message, Severity severity)
+        {
+            MessageWindow.Show(_mainWindow, title, message, severity, MessageWindowButtons.Ok);
+        }
+
+        public override string Ask(string title, string message, string defaultValue)
+        {
+            var window = new InputWindow()
+            {
+                Owner = _mainWindow,
+                Title = "Save Template",
+                Message = "Please enter a name for the template:",
+                Value = defaultValue,
+                IsValueRequired = true
+            };
+
+            return window.ShowDialog() == true ? window.Value : null;
+        }
+
+        public override bool Confirm(string title, string message)
+        {
+            return MessageWindow.Show(_mainWindow, title, message, Severity.Warning, MessageWindowButtons.OkCancel) == true;
+        }
+    }
+}
---
+++ Tricycle.UI.Windows/MessageWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Tricycle.UI.Windows
+{
+    public enum MessageWindowButtons
+    {
+        Ok,
+        OkCancel
+    }
+
+    /// <summary>
+    /// Interaction logic for MessageWindow.xaml
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        Severity _severity = Severity.Info;
+        MessageWindowButtons _buttons = MessageWindowButtons.Ok;
+
+        public MessageWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string Message
+        {
+            get => txtMessage.Text?.ToString();
+            set => txtMessage.Text = value;
+        }
+
+        public Severity Severity
+        {
+            get => _severity;
+            set
+            {
+                _severity = value;
+
+                string filename;
+
+                switch (value)
+                {
+                    case Severity.Info:
+                    default:
+                        filename =  @"Assets\Images\info.png";
+                        break;
+                    case Severity.Warning:
+                        filename = @"Assets\Images\warning.png";
+                        break;
+                    case Severity.Error:
+                        filename = @"Assets\Images\error.png";
+                        break;
+                }
+
+                imgSeverity.Source = new BitmapImage(new Uri(filename, UriKind.Relative));
+            }
+        }
+
+        public MessageWindowButtons Buttons
+        {
+            get => _buttons;
+            set
+            {
+                _buttons = value;
+
+                btnCancel.Visibility = value == MessageWindowButtons.OkCancel ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        public static bool? Show(Window owner, string title, string message, Severity severity, MessageWindowButtons buttons)
+        {
+            var window = new MessageWindow()
+            {
+                Owner = owner,
+                Title = title,
+                Message = message,
+                Severity = severity,
+                Buttons = buttons
+            };
+
+            return window.ShowDialog();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            IconHelper.RemoveIcon(this);
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
