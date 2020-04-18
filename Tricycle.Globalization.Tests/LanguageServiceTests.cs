d38f9f191f3abb65da57aa937c8e8bca8673acab
Kenny McClive <kenny.mcclive@me.com>
2020-04-18 13:34:25 -0600

    Implemented LanguageService.

---
+++ Tricycle.Globalization.Tests/LanguageServiceTests.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tricycle.Globalization.Tests
+{
+    [TestClass]
+    public class LanguageServiceTests
+    {
+        LanguageService _service;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _service = new LanguageService();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FindThrowsExceptionWhenCodeIsNull()
+        {
+            _service.Find(null);
+        }
+
+        [TestMethod]
+        public void FindReturnsNullForEmptyCode()
+        {
+            Assert.IsNull(_service.Find(string.Empty));
+        }
+
+        [TestMethod]
+        public void FindReturnsLanguageForPart1()
+        {
+            var language = _service.Find("fr");
+
+            Assert.AreEqual("French", language?.Name);
+        }
+
+        [TestMethod]
+        public void FindReturnsLanguageForPart2()
+        {
+            var language = _service.Find("fra");
+
+            Assert.AreEqual("French", language?.Name);
+        }
+
+        [TestMethod]
+        public void FindReturnsLanguageForPart2B()
+        {
+            var language = _service.Find("fre");
+
+            Assert.AreEqual("French", language?.Name);
+        }
+
+        [TestMethod]
+        public void FindReturnsLanguageForPart3()
+        {
+            var language = _service.Find("hbo");
+
+            Assert.AreEqual("Ancient Hebrew", language?.Name);
+        }
+    }
+}
---
+++ Tricycle.Globalization/LanguageService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Iso639;
+
+namespace Tricycle.Globalization
+{
+    public class LanguageService : ILanguageService
+    {
+        public Language Find(string code)
+        {
+            if (code == null)
+            {
+                throw new ArgumentNullException(nameof(code));
+            }
+
+            if (string.IsNullOrWhiteSpace(code))
+            {
+                return null;
+            }
+
+            if (code.Length == 2)
+            {
+                return Language.Database.FirstOrDefault(l => code.Equals(l.Part1, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (code.Length == 3)
+            {
+                return Language.Database.FirstOrDefault(l =>
+                    code.Equals(l.Part3, StringComparison.OrdinalIgnoreCase) ||
+                    code.Equals(l.Part2, StringComparison.OrdinalIgnoreCase) ||
+                    code.Equals(l.Part2B, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return null;
+        }
+    }
+}
