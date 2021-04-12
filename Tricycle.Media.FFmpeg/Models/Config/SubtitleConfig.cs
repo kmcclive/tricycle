1aa43fd4e3f0e6dc5f736fa0ae86be4fb7789867
Kenny McClive <kenny.mcclive@me.com>
2021-04-12 17:35:21 -0600

    Added subtitles to FFmpeg config.

---
+++ Tricycle.Media.FFmpeg/Models/Config/SubtitleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tricycle.Models;
+
+namespace Tricycle.Media.FFmpeg.Models.Config
+{
+    public class SubtitleConfig
+    {
+        public IDictionary<SubtitleFormat, SubtitleCodec> Codecs { get; set; }
+
+        public SubtitleConfig Clone()
+        {
+            return new SubtitleConfig()
+            {
+                Codecs = Codecs?.ToDictionary(p => p.Key, p => p.Value?.Clone())
+            };
+        }
+    }
+}
