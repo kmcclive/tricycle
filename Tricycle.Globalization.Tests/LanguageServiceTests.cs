911c2c12fe72a4b15a6f87d3982d029ded5f2458
Kenny McClive <kmcclive@captechconsulting.com>
2022-05-20 08:16:01 -0600

    Upgraded Tricycle.Globalization.Tests to .NET 6.

--- Tricycle.Globalization.Tests/LanguageServiceTests.cs
+++ Tricycle.Globalization.Tests/LanguageServiceTests.cs
@@ -1,87 +1,84 @@
 using System;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tricycle.Globalization.Models;
 
-namespace Tricycle.Globalization.Tests
+namespace Tricycle.Globalization.Tests;
+
+[TestClass]
+public class LanguageServiceTests
 {
-    [TestClass]
-    public class LanguageServiceTests
+    static readonly Language FRENCH = new Language("French", "fra", "fre", "fra", "fr");
+    static readonly Language HEBREW = new Language("Ancient Hebrew", "hbo", null, null, null);
+
+    LanguageService _service;
+
+    [TestInitialize]
+    public void Setup()
     {
-        static readonly Language FRENCH = new Language("French", "fra", "fre", "fra", "fr");
-        static readonly Language HEBREW = new Language("Ancient Hebrew", "hbo", null, null, null);
+        _service = new LanguageService();
+    }
 
-        LanguageService _service;
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void FindThrowsExceptionWhenCodeIsNull()
+    {
+        _service.Find(null);
+    }
 
-        [TestInitialize]
-        public void Setup()
-        {
-            _service = new LanguageService();
-        }
+    [TestMethod]
+    public void FindReturnsNullForEmptyCode()
+    {
+        Assert.IsNull(_service.Find(string.Empty));
+    }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void FindThrowsExceptionWhenCodeIsNull()
-        {
-            _service.Find(null);
-        }
+    [TestMethod]
+    public void FindReturnsLanguageForPart1()
+    {
+        var language = _service.Find(FRENCH.Part1);
 
-        [TestMethod]
-        public void FindReturnsNullForEmptyCode()
-        {
-            Assert.IsNull(_service.Find(string.Empty));
-        }
+        Assert.IsNotNull(language);
+        Assert.AreEqual(FRENCH.Name, language.Name);
+        Assert.AreEqual(FRENCH.Part3, language.Part3);
+        Assert.AreEqual(FRENCH.Part2B, language.Part2B);
+        Assert.AreEqual(FRENCH.Part2, language.Part2);
+        Assert.AreEqual(FRENCH.Part1, language.Part1);
+    }
 
-        [TestMethod]
-        public void FindReturnsLanguageForPart1()
-        {
-            var language = _service.Find(FRENCH.Part1);
+    [TestMethod]
+    public void FindReturnsLanguageForPart2()
+    {
+        var language = _service.Find(FRENCH.Part2);
 
-            Assert.IsNotNull(language);
-            Assert.AreEqual(FRENCH.Name, language.Name);
-            Assert.AreEqual(FRENCH.Part3, language.Part3);
-            Assert.AreEqual(FRENCH.Part2B, language.Part2B);
-            Assert.AreEqual(FRENCH.Part2, language.Part2);
-            Assert.AreEqual(FRENCH.Part1, language.Part1);
-        }
+        Assert.IsNotNull(language);
+        Assert.AreEqual(FRENCH.Name, language.Name);
+        Assert.AreEqual(FRENCH.Part3, language.Part3);
+        Assert.AreEqual(FRENCH.Part2B, language.Part2B);
+        Assert.AreEqual(FRENCH.Part2, language.Part2);
+        Assert.AreEqual(FRENCH.Part1, language.Part1);
+    }
 
-        [TestMethod]
-        public void FindReturnsLanguageForPart2()
-        {
-            var language = _service.Find(FRENCH.Part2);
+    [TestMethod]
+    public void FindReturnsLanguageForPart2B()
+    {
+        var language = _service.Find(FRENCH.Part2B);
 
-            Assert.IsNotNull(language);
-            Assert.AreEqual(FRENCH.Name, language.Name);
-            Assert.AreEqual(FRENCH.Part3, language.Part3);
-            Assert.AreEqual(FRENCH.Part2B, language.Part2B);
-            Assert.AreEqual(FRENCH.Part2, language.Part2);
-            Assert.AreEqual(FRENCH.Part1, language.Part1);
-        }
+        Assert.IsNotNull(language);
+        Assert.AreEqual(FRENCH.Name, language.Name);
+        Assert.AreEqual(FRENCH.Part3, language.Part3);
+        Assert.AreEqual(FRENCH.Part2B, language.Part2B);
+        Assert.AreEqual(FRENCH.Part2, language.Part2);
+        Assert.AreEqual(FRENCH.Part1, language.Part1);
+    }
 
-        [TestMethod]
-        public void FindReturnsLanguageForPart2B()
-        {
-            var language = _service.Find(FRENCH.Part2B);
+    [TestMethod]
+    public void FindReturnsLanguageForPart3()
+    {
+        var language = _service.Find(HEBREW.Part3);
 
-            Assert.IsNotNull(language);
-            Assert.AreEqual(FRENCH.Name, language.Name);
-            Assert.AreEqual(FRENCH.Part3, language.Part3);
-            Assert.AreEqual(FRENCH.Part2B, language.Part2B);
-            Assert.AreEqual(FRENCH.Part2, language.Part2);
-            Assert.AreEqual(FRENCH.Part1, language.Part1);
-        }
-
-        [TestMethod]
-        public void FindReturnsLanguageForPart3()
-        {
-            var language = _service.Find(HEBREW.Part3);
-
-            Assert.IsNotNull(language);
-            Assert.AreEqual(HEBREW.Name, language.Name);
-            Assert.AreEqual(HEBREW.Part3, language.Part3);
-            Assert.AreEqual(HEBREW.Part2B, language.Part2B);
-            Assert.AreEqual(HEBREW.Part2, language.Part2);
-            Assert.AreEqual(HEBREW.Part1, language.Part1);
-        }
+        Assert.IsNotNull(language);
+        Assert.AreEqual(HEBREW.Name, language.Name);
+        Assert.AreEqual(HEBREW.Part3, language.Part3);
+        Assert.AreEqual(HEBREW.Part2B, language.Part2B);
+        Assert.AreEqual(HEBREW.Part2, language.Part2);
+        Assert.AreEqual(HEBREW.Part1, language.Part1);
     }
 }
